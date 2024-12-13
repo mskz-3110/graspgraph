14680e00246d2eeb8598b085e54d00dae40ec1d9
mskz-3110 <mskz.saito@gmail.com>
2024-12-13 15:08:47 +0900

    Update dbergraph

--- src/graspgraph/__init__.py
+++ src/graspgraph/__init__.py
@@ -2,6 +2,7 @@ from .array import *
 from .statsgraph import *
 from .figure import *
 from .color import *
+from .database import *
 from .dbergraph import *
 from .dot import *
 from .font import *
